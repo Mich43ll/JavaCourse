552699258988fab640eb49fc6f1f722ce35f2be3
Michaell Osorio <mosoriobaralona@outlook.com>
2022-07-28 21:13:23 -0600

    first commit

---
+++ HolaMundo/src/HolaMundo.java
@@ -0,0 +1,25 @@
+//Mi Clase en Java
+public class HolaMundo {
+    public static void main(String args[]){
+        //Definimos la variable
+        int miVariableEntera = 10;
+        System.out.println(miVariableEntera);
+        //Modificamoselvalor de la variable
+        miVariableEntera = 5;
+        System.out.println(miVariableEntera);
+        
+        String miVariableCadena = "Saludos";
+        System.out.println(miVariableCadena);
+        
+        miVariableCadena ="no se";
+        System.out.println(miVariableCadena);
+        
+        //var -inferencia de tipos en Java
+        var miVariableEntera2=15;
+        System.out.println(miVariableEntera2);
+        
+        var miVariableCadena2 = "Adios";
+        System.out.println("miVariableCadena2 = " + miVariableCadena2);
+        
+    }
+}
---
+++ Hola_mundo/src/hola_mundo/Hola_mundo.java
@@ -0,0 +1,8 @@
+//Mi clase en java
+public class Hola_mundo {
+    public static void main(String[] args) {
+        int miVariableEntera =10;
+        System.out.println(miVariableEntera);
+    }
+    
+}

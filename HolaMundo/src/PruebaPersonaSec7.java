49fc6bb86e99db62d5797a7b2f0a3355d82a8eb8
Mich43ll <mosoriobarahona@gmail.com>
2022-08-31 21:21:19 -0600

    Seccion 7

---
+++ HolaMundo/src/PruebaPersonaSec7.java
@@ -0,0 +1,14 @@
+public class PruebaPersonaSec7 {
+    public static void main(String[] args) {
+        //Objeto
+        Seccion7 persona1 = new Seccion7();
+        persona1.nombre = "Juan";
+        persona1.apellido = "Perez";
+        persona1.desplegarInformacio();
+
+        Seccion7 persona2 = new Seccion7();
+        persona2.nombre ="Karla";
+        persona2.apellido = "Lara";
+        persona2.desplegarInformacio();
+    }
+}
---
+++ HolaMundo/src/Seccion7.java
@@ -0,0 +1,11 @@
+public class Seccion7 {
+    //Atributos de la clase 
+    String nombre;
+    String apellido;
+
+    public void desplegarInformacio(){
+        System.out.println("Nombre: "+nombre);
+        System.out.println("Apellido: "+apellido);
+    }
+
+}

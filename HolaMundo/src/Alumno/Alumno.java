694b4901726c4118654f6eb5dfbadde9c62034cc
Mich43ll <mosoriobarahona@gmail.com>
2022-08-31 21:53:06 -0600

    Practica de Clases

---
+++ HolaMundo/src/Alumno/Alumno.java
@@ -0,0 +1,11 @@
+package Alumno;
+
+public class Alumno {
+    public static void main(String[] args) {
+        CL_Alumno alumno1 = new CL_Alumno();
+        alumno1.notaTotal();
+
+        CL_Alumno alumno2 = new CL_Alumno();
+        alumno2.notaTotal();
+    }
+}
---
+++ HolaMundo/src/Alumno/CL_Alumno.java
@@ -0,0 +1,26 @@
+package Alumno;
+
+import java.util.Scanner;
+
+public class CL_Alumno {
+    int nota;
+    int acumulador = 0; 
+    String nombre; 
+    String apellido;
+
+    public void notaTotal(){
+        var scaner = new Scanner(System.in);
+        System.out.println("Ingrese su nombre:");
+        nombre = scaner.nextLine();
+        System.out.println("Ingrese su apellido");
+        apellido = scaner.nextLine();
+        System.out.println("");
+        for(var i = 1; i < 4; i++){
+            System.out.println("Ingrese la nota "+i+":");
+            nota = Integer.parseInt(scaner.nextLine());
+            acumulador = acumulador + nota;
+        }
+        System.out.println(nombre+" "+apellido);
+        System.out.println("El promedio total es: "+acumulador/3);
+    }
+}

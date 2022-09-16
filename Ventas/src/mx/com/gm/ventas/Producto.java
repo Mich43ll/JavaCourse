3d9bc27d5ca6c9ea32cb4899987bb0f96b63888d
Mich43ll <mosoriobarahona@gmail.com>
2022-09-15 21:56:15 -0600

    sistema de Ventas con Java

---
+++ Ventas/src/mx/com/gm/ventas/Producto.java
@@ -0,0 +1,44 @@
+package mx.com.gm.ventas;
+
+public class Producto {
+    private final int idProducto;
+    private String nombre;
+    private double precio;
+    private static int contadorProductos;
+
+    private Producto() {
+        this.idProducto = ++Producto.contadorProductos;
+    }
+
+    public Producto(String nombre, double precio) {
+        this();
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public int getIdProducto() {
+        return idProducto;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return "idProducto " + idProducto + ", nombre " + nombre + ", precio " + precio;
+    }
+
+}
